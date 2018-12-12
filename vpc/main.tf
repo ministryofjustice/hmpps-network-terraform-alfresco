cd13e12dddc3b288e5b033bed2c241966d38f69b
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-12-12 16:33:49 +0000

    Added monitoring node

--- vpc/main.tf
+++ vpc/main.tf
@@ -12,3 +12,17 @@ provider "aws" {
 ### Getting the current running account id
 #-------------------------------------------------------------
 data "aws_caller_identity" "current" {}
+
+#-------------------------------------------------------------
+### Getting the bastion vpc
+#-------------------------------------------------------------
+data "terraform_remote_state" "bastion_remote_vpc" {
+  backend = "s3"
+
+  config {
+    bucket   = "${var.bastion_remote_state_bucket_name}"
+    key      = "bastion-vpc/terraform.tfstate"
+    region   = "${var.region}"
+    role_arn = "${var.bastion_role_arn}"
+  }
+}
