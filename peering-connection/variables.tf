e8513d28f12c10687c25750db97c9742ae329db1
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-12-14 14:49:21 +0000

    Added perring and renamed monitoring module

---
+++ peering-connection/variables.tf
@@ -0,0 +1,19 @@
+variable "environment_name" {
+  type = "string"
+}
+
+variable "tags" {
+  type = "map"
+}
+
+variable "region" {
+  description = "The AWS region."
+}
+
+variable "remote_state_bucket_name" {
+  description = "Terraform remote state bucket name"
+}
+
+variable "environment_identifier" {
+  description = "resource label or name"
+}
--- vpc/main.tf
+++ vpc/main.tf
@@ -14,6 +14,20 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 
 #-------------------------------------------------------------
+### Getting the engineering platform vpc
+#-------------------------------------------------------------
+data "terraform_remote_state" "eng_remote_vpc" {
+  backend = "s3"
+
+  config {
+    bucket   = "${var.eng_remote_state_bucket_name}"
+    key      = "vpc/terraform.tfstate"
+    region   = "${var.region}"
+    role_arn = "${var.eng_role_arn}"
+  }
+}
+
+#-------------------------------------------------------------
 ### Getting the bastion vpc
 #-------------------------------------------------------------
 data "terraform_remote_state" "bastion_remote_vpc" {
