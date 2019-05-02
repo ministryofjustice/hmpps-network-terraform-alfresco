dd4dd7cbe9fc1876ce2777f1351c3475a1835e11
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2019-05-02 12:42:45 +0100

    Add monitoring security groups

---
+++ security-groups/shared_monitoring.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "monitoring_sg" {
+  name        = "${var.environment_identifier}-monitoring-elk"
+  description = "security group for ${var.environment_identifier}-monitoring"
+  vpc_id      = "${data.terraform_remote_state.vpc.vpc_id}"
+
+  tags = "${merge(data.terraform_remote_state.vpc.tags, map("Name", "${var.environment_identifier}-monitoring-elk"))}"
+}
+
+resource "aws_security_group" "monitoring_elb_sg" {
+  name        = "${var.environment_identifier}-monitoring-elk-elb"
+  description = "security group for ${var.environment_identifier}-monitoring-elk-elb"
+  vpc_id      = "${data.terraform_remote_state.vpc.vpc_id}"
+  tags        = "${merge(data.terraform_remote_state.vpc.tags, map("Name", "${var.environment_identifier}-monitoring-elk-elb"))}"
+}
+
+# outputs
+output "sg_monitoring" {
+  value = "${aws_security_group.monitoring_sg.id}"
+}
+
+output "sg_monitoring_elb" {
+  value = "${aws_security_group.monitoring_elb_sg.id}"
+}
