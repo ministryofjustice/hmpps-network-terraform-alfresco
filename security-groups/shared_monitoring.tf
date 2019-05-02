58affce9c8977f23b51c8230a865417e3703ddcd
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2019-05-02 15:39:03 +0100

    LB deployed

--- security-groups/shared_monitoring.tf
+++ security-groups/shared_monitoring.tf
@@ -13,6 +13,14 @@ resource "aws_security_group" "monitoring_elb_sg" {
   tags        = "${merge(data.terraform_remote_state.vpc.tags, map("Name", "${var.environment_identifier}-monitoring-elk-elb"))}"
 }
 
+resource "aws_security_group" "monitoring_client_sg" {
+  name        = "${var.environment_identifier}-monitoring-elk-client"
+  description = "security group for ${var.environment_identifier}-elasticsearch"
+  vpc_id      = "${data.terraform_remote_state.vpc.vpc_id}"
+
+  tags = "${merge(data.terraform_remote_state.vpc.tags, map("Name", "${var.environment_identifier}-monitoring-elk-client"))}"
+}
+
 # outputs
 output "sg_monitoring" {
   value = "${aws_security_group.monitoring_sg.id}"
@@ -20,4 +28,8 @@ output "sg_monitoring" {
 
 output "sg_monitoring_elb" {
   value = "${aws_security_group.monitoring_elb_sg.id}"
+}
+
+output "sg_monitoring_client" {
+  value = "${aws_security_group.monitoring_client_sg.id}"
 }
