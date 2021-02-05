b72d9cde60525f55247a6e88ca48eb30f88aa621
Don Nyambudzi <don.nyambudzi@gmail.com>
2021-02-05 15:35:07 +0000

    Updated peering code

--- peering-connection/peering-bastion.tf
+++ peering-connection/peering-bastion.tf
@@ -12,27 +12,6 @@ resource "aws_vpc_peering_connection" "peering-bastion-vpc" {
   )
 }
 
-## Another way do it
-# module "route-to-bastion-vpc-cidr" {
-#   source = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//routes//vpc_peer"
-#
-#   route_table_id = [
-#     "${data.terraform_remote_state.vpc.vpc_public-routetable-az1}",
-#     "${data.terraform_remote_state.vpc.vpc_public-routetable-az2}",
-#     "${data.terraform_remote_state.vpc.vpc_public-routetable-az3}",
-#     "${data.terraform_remote_state.vpc.vpc_private-routetable-az1}",
-#     "${data.terraform_remote_state.vpc.vpc_private-routetable-az2}",
-#     "${data.terraform_remote_state.vpc.vpc_private-routetable-az3}",
-#     "${data.terraform_remote_state.vpc.vpc_db-routetable-az1}",
-#     "${data.terraform_remote_state.vpc.vpc_db-routetable-az2}",
-#     "${data.terraform_remote_state.vpc.vpc_db-routetable-az3}",
-#   ]
-#
-#   destination_cidr_block = "${data.terraform_remote_state.bastion_remote_vpc.vpc.cidr}"
-#   vpc_peer_id            = "${aws_vpc_peering_connection.peering-bastion-vpc.id}"
-#   create                 = 1
-# }
-
 module "route-to-bastion-vpc-public-cidr-az1" {
   source = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git//modules/routes/vpc_peer?ref=terraform-0.12"
 
@@ -50,7 +29,7 @@ module "route-to-bastion-vpc-public-cidr-az1" {
 
   destination_cidr_block = data.terraform_remote_state.bastion_remote_vpc.outputs.vpc.public_cidr.az1
   vpc_peer_id            = aws_vpc_peering_connection.peering-bastion-vpc.id
-  create                 = 1
+  create                 = true
 }
 
 module "route-to-bastion-vpc-public-cidr-az2" {
@@ -70,7 +49,7 @@ module "route-to-bastion-vpc-public-cidr-az2" {
 
   destination_cidr_block = data.terraform_remote_state.bastion_remote_vpc.outputs.vpc.public_cidr.az2
   vpc_peer_id            = aws_vpc_peering_connection.peering-bastion-vpc.id
-  create                 = 1
+  create                 = true
 }
 
 module "route-to-bastion-vpc-public-cidr-az3" {
@@ -90,7 +69,7 @@ module "route-to-bastion-vpc-public-cidr-az3" {
 
   destination_cidr_block = data.terraform_remote_state.bastion_remote_vpc.outputs.vpc.public_cidr.az3
   vpc_peer_id            = aws_vpc_peering_connection.peering-bastion-vpc.id
-  create                 = 1
+  create                 = true
 }
 
 # ## Route to bastion private
@@ -112,7 +91,7 @@ module "route-to-bastion-vpc-private-cidr-az1" {
 
   destination_cidr_block = data.terraform_remote_state.bastion_remote_vpc.outputs.vpc.private_cidr.az1
   vpc_peer_id            = aws_vpc_peering_connection.peering-bastion-vpc.id
-  create                 = 1
+  create                 = true
 }
 
 module "route-to-bastion-vpc-private-cidr-az2" {
@@ -132,7 +111,7 @@ module "route-to-bastion-vpc-private-cidr-az2" {
 
   destination_cidr_block = data.terraform_remote_state.bastion_remote_vpc.outputs.vpc.private_cidr.az2
   vpc_peer_id            = aws_vpc_peering_connection.peering-bastion-vpc.id
-  create                 = 1
+  create                 = true
 }
 
 module "route-to-bastion-vpc-private-cidr-az3" {
@@ -152,6 +131,6 @@ module "route-to-bastion-vpc-private-cidr-az3" {
 
   destination_cidr_block = data.terraform_remote_state.bastion_remote_vpc.outputs.vpc.private_cidr.az3
   vpc_peer_id            = aws_vpc_peering_connection.peering-bastion-vpc.id
-  create                 = 1
+  create                 = true
 }
 
